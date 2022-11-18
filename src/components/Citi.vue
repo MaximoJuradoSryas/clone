a97b427d4109110e6562a4647a5343550f6968ec
MaximoJuradoSryas <Maximo.Jurado@sryas.com>
2022-11-18 09:25:30 -0600

    firts commit

---
+++ src/components/Citi.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="header">
+      <div class="header-wrapp">
+        <div class="logo">
+            <img src="https://www.citi.com/CBOL/IA/Angular/assets/citilogoredesign.png" >
+                <div class="cajaubi">
+                <p class="ubi"><img src="https://cdn-icons-png.flaticon.com/512/535/535239.png" width="30" height="30" ><br>
+                ATM/BRANCH
+                </p>
+                </div>
+                
+                    <div class="cajatra">
+                        <p class="tradu"><img src="https://cdn-icons-png.flaticon.com/512/900/900782.png" width="30" height="30" ><br>
+                        ESPAÑOL
+                        </p>
+                    </div>
+        </div>
+      </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.header{
+    width: 100%;
+    height: 100px;
+    display: flex;
+}
+
+.header-wrapp{
+    width: 100%;
+    padding: 0 35px 0 20px;
+    display: grid;
+    grid-auto-rows: 4fr;
+    justify-content: space-between;
+    
+}
+.cajaubi{
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    
+
+}
+
+.ubi{
+    color:gray;
+    margin: 10px;
+
+}
+.tradu{
+    color: gray;
+    margin: 10px;
+}
+
+
+.logo{
+    display: flex;
+    justify-content: start;
+    margin: 3px;
+
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Citi msg="Citi banamax"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Citi from '@/components/Citi.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Citi
+  }
+}
+
+    Citi</script>
